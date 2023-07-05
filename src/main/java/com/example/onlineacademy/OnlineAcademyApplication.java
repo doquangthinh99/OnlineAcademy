8fc9b3c4896a1ac4f47c6b6a31925a7264645f79
Đỗ Quang Thịnh <quangthinh123321@gmail.com>
2023-07-05 09:19:07 +0700

    + Create project

---
+++ src/main/java/com/example/onlineacademy/OnlineAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineAcademyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onlineacademy/OnlineAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
