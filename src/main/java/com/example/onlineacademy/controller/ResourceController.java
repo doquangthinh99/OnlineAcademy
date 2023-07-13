4931881b425ffdd70866f2ead0177aa8cdec8c61
Đỗ Quang Thịnh <quangthinh123321@gmail.com>
2023-07-13 09:26:38 +0700

    * Add API get resource

---
+++ src/main/java/com/example/onlineacademy/controller/ResourceController.java
@@ -0,0 +1,38 @@
+package com.example.onlineacademy.controller;
+
+import org.aspectj.util.FileUtil;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class ResourceController {
+    @GetMapping("/image")
+    @ResponseBody
+    public HttpEntity<byte[]> getPhoto(@RequestParam("filename") String filename) {
+        byte[] image = new byte[0];
+        if(!filename.equals("undefined")) {
+            InputStream is = null;
+            try {
+                is = new ClassPathResource("/data/" + filename).getInputStream();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                image = FileUtil.readAsByteArray(is);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        headers.setContentLength(image.length);
+        return new HttpEntity<byte[]>(image, headers);
+    }
+}
